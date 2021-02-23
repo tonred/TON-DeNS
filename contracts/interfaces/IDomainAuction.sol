e2220c714ab773a74fc70ccd6ab5ab67a9076345
Abionics <abionics.dev@gmail.com>
2021-02-23 11:26:26 +0200

    Create interface for domain auction

--- contracts/interfaces/IDomainAuction.sol
+++ contracts/interfaces/IDomainAuction.sol
@@ -2,7 +2,29 @@ pragma solidity ^0.4.0;
 
 
 interface IDomainAuction {
+    /* Getters */
+    function getAddressNIC() external pure returns (address);
+
+    function getRelativeDomainName() external pure returns (string);
+
+    function getDomainRegisterDuration() external pure returns (uint32);
+
+    function getPhase() external pure returns (Phase);
+
+    function getOpenTime() external pure returns (PhaseTime);
+
+    function getConfirmationTime() external pure returns (PhaseTime);
+
+    function getCloseTime() external pure returns (PhaseTime);
+
+    /* Bids functions */
+    function makeBid(string bidHash) external returns (string);
+
+    function removeBid(string bidHash) external returns (string);
+
+    function confirmBid(uint64 bid, uint256 salt) external returns (string);
+
+    function update() external;
+
+    /* History functions ? */
 }
-
-
-
